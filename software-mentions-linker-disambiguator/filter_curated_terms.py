706ba713a4586fdb72bd5f3616a4dd8566f37863
anaistrate <aistrate@chanzuckerberg.com>
2022-07-10 14:16:03 -0700

    Saving curated datasets as gzip files, minor issue in filtering based on IDs

--- software-mentions-linker-disambiguator/filter_curated_terms.py
+++ software-mentions-linker-disambiguator/filter_curated_terms.py
@@ -13,7 +13,7 @@ import pandas as pd
 import argparse
 import pickle
 
-ROOT_DIR = "/data"
+ROOT_DIR = "data/"
 
 def get_curation_label(ID, software_IDs, not_software_IDs, unclear_IDs):
   """
@@ -37,25 +37,38 @@ def get_curation_label(ID, software_IDs, not_software_IDs, unclear_IDs):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   
-  parser.add_argument("--software-mentions-file", type=str, help="Input file", default = ROOT_DIR + 'comm_IDs.tsv', required = False)
+  parser.add_argument("--software-mentions-file", type=str, help="Input file", default = ROOT_DIR + 'comm_IDs.tsv.gz', required = False)
   parser.add_argument("--curated-terms", type=str, help="File containing curated terms", default = ROOT_DIR + 'curation_top10k_mentions_binary_labels.csv', required = False)
   parser.add_argument("--mention2ID-file", type=str, help="mention2ID mapping", default = ROOT_DIR + 'mention2ID.pkl', required = False)
-  parser.add_argument("--output-curated-dataset", type=str, help="Output file for curated dataset", default = ROOT_DIR + 'comm_IDs_curated.csv', required = False)
-  parser.add_argument("--output-augmented-dataset", type=str, help="Output file for augemented dataset", default = ROOT_DIR + 'comm_IDs_augmented.csv', required = False)
+  parser.add_argument("--output-curated-dataset", type=str, help="Output file for curated dataset", default = ROOT_DIR + 'comm_curated.tsv.gz', required = False)
+  parser.add_argument("--output-augmented-dataset", type=str, help="Output file for augemented dataset", default = ROOT_DIR + 'comm_with_labels.tsv.gz', required = False)
 
   args, _ = parser.parse_known_args()
   raw_mentions_df = pd.read_csv(args.software_mentions_file, sep = '\\t', engine = 'python', compression = 'gzip')
   curated_terms = pd.read_csv(args.curated_terms)
   mention2ID = pickle.load(open(args.mention2ID_file, 'rb'))
-  software_IDs = curated_terms[curated_terms['label'] == 'software']['ID'].values
-  not_software_IDs = curated_terms[curated_terms['label'] == 'not_software']['ID'].values
-  unclear_IDs = curated_terms[curated_terms['label'] == 'unclear']['ID'].values
+  software_IDs = set(curated_terms[curated_terms['label'] == 'software']['ID'].values)
+  not_software_IDs = set(curated_terms[curated_terms['label'] == 'not_software']['ID'].values)
+  unclear_IDs = set(curated_terms[curated_terms['label'] == 'unclear']['ID'].values)
   
   # Exclude terms marked as not_software
   curated_mentions_df = raw_mentions_df.copy()
   curated_mentions_df = curated_mentions_df[~(curated_mentions_df['ID'].isin(not_software_IDs))]
-  curated_mentions_df.to_csv(args.output_curated_dataset)
+  curated_mentions_df.to_csv(args.output_curated_dataset, sep = '\t', compression = 'gzip', index = False)
 
   # Adds a curation label to the raw dataset and saves to file
-  raw_mentions_df['curation_label'] = raw_mentions_df['ID'].apply(lambda x: get_curation_label(x, software, not_software, unclear))
-  raw_mentions_df.to_csv(args.output_augmented_dataset, sep = '\t')
+  raw_mentions_df['curation_label'] = raw_mentions_df['ID'].apply(lambda x: get_curation_label(x, software_IDs, not_software_IDs, unclear_IDs))
+  
+  # Print metrics
+  dataset_software = len(raw_mentions_df[raw_mentions_df['curation_label'] == 'software'])
+  dataset_not_software = len(raw_mentions_df[raw_mentions_df['curation_label'] == 'not_software'])
+  dataset_unclear = len(raw_mentions_df[raw_mentions_df['curation_label'] == 'unclear'])
+  dataset_not_curated = len(raw_mentions_df[raw_mentions_df['curation_label'] == 'not_curated'])
+  num_total = len(raw_mentions_df)
+
+  print('% software:', (dataset_software / num_total) * 100)
+  print('% not-software:', (dataset_not_software / num_total) * 100)
+  print('% unclear:', (dataset_unclear / num_total) * 100)
+  print('% not-curated:', (dataset_not_curated / num_total) * 100)
+  
+  raw_mentions_df.to_csv(args.output_augmented_dataset, sep = '\t', compression = 'gzip', index = False)
